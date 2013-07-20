4e3793129e463dd87876be24bfcc1601e38c877a
Gabriel Ratener <gabe9693@gmail.com>
2013-07-20 09:31:10 -0700

    Extended Element.js

--- src/dependencies/Element.js
+++ src/dependencies/Element.js
@@ -1,6 +1,6 @@
 function Element(elem){
 	var eleme = document.createElement(elem);
-	var myobj = {};
+	var myobj = {}, binder;
 
 	this.obj = function(){
 		return myobj;
@@ -9,9 +9,18 @@ function Element(elem){
 	this.get = function(){
 		return eleme;
 	}
+
+	this.bind = function(obje, override){
+		if(!binder || override){
+			binder = obje;
+			return true;
+		}
+
+		return false;
+	}
 }
 
-Element.prototype.append = function(name, el){
+Element.prototype.append = function(name, el, truth){
 	if (arguments.length < 2) var el = name;
 	
 	if(this[name]){
@@ -24,7 +33,8 @@ Element.prototype.append = function(name, el){
 			this[name] = temp;
 		}	
 	}else{
-		var r = this[name] = new Element(el);
+		var r = new Element(el);
+		this[name] = (truth) ? [r] : r;
 	}
 	
 	this.get().appendChild(r.get());
@@ -62,4 +72,32 @@ Element.prototype.takeClass = function(){
 	
 	elem.setAttribute("class", arey.join(" "));
 	return this;
+}
+
+Element.prototype.attributes = function(){
+	var el = this.get();
+
+	if(arguments.length){
+		var obj = arguments[0];
+
+		for (var k in obj) {
+			el.setAttribute(k, obj[k]);
+		}
+
+		return this;
+	}else{
+		var obj = {}
+		for(var i = 0; i < el.attributes.length; i++){
+			obj[el.attributes[i].name] = el.attributes[i].value;
+		}
+
+		return obj;
+	}
+}
+
+Element.prototype.text = function(str){
+	var el = this.get();
+	var tn = document.createTextNode(str);
+
+	el.appendChild(tn);
 }
